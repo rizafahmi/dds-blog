a45a8f66cde2c546f436f281bde7938152987480
Riza Fahmi <rizafahmi@gmail.com>
2016-05-22 19:44:44 +0700

    Finish building list of post

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule DdsBlog.PageController do
   use DdsBlog.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    {:ok, posts} = DdsBlog.Repo.list()
+    render conn, "index.html", posts: posts
   end
 end
